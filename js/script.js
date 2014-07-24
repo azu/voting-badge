b82761b0b04b1348c62a2cdc707554f75ea91703
azu <azuciao@gmail.com>
2014-07-24 21:08:18 +0900

    update event

--- js/script.js
+++ js/script.js
@@ -8,8 +8,14 @@ function convertURL(url) {
         + url
         + ")"
 }
-input.addEventListener("input", function (evt) {
+function updateOutput() {
     var url = input.value;
     console.log(url);
     output.value = convertURL(url);
+}
+input.addEventListener("keyup", function (evt) {
+    updateOutput();
+});
+input.addEventListener("paste", function (evt) {
+    setTimeout(updateOutput, 0);
 });
